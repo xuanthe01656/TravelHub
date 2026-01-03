ecf55455cbe2d924877a16bd4eeb84afb59563e8
Nguyen Xuan The <xuanthe01656@gmail.com>
2026-01-03 13:16:32 +0700

    Update

--- server/server.js
+++ server/server.js
@@ -22,7 +22,8 @@ const axios = require("axios");
 const NodeCache = require('node-cache');
 const myCache = new NodeCache({ stdTTL: 1800, checkperiod: 600 });
 app.set("trust proxy", 1);
-app.use(express.static(path.join(__dirname, '..')));
+const buildPath = path.join(__dirname, '..', 'dist');
+app.use(express.static(buildPath));
 const {
   getUsers,
   addUser,
@@ -937,7 +938,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Internal Server Error' });
 });
-app.use((req, res) => { res.sendFile(path.join(__dirname, '..', 'index.html')); });
+app.use((req, res) => { res.sendFile(path.join(buildPath, 'index.html')); });
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
