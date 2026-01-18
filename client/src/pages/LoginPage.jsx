2e626d1f9923dd8172009c05ede2015a25de6797
Nguyen Xuan The <xuanthe01656@gmail.com>
2026-01-19 06:14:36 +0700

    Update

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -32,26 +32,26 @@ function Login() {
   //const navigate = useNavigate();
   useDocumentTitle('Đăng nhập');
 
-  // useEffect(() => {
-  //   if (isRedirecting) return;
-  //   const checkSession = async () => {
-  //     try {
-  //       const response = await axios.get('/api/session');
-  //       if (response.data && typeof response.data === 'object' && response.data.loggedIn === true) {
-  //         console.log("Đã đăng nhập, chuyển sang Dashboard");
-  //         navigate('/dashboard');
-  //       } else {
-  //         console.log("Chưa đăng nhập, ở lại trang Login");
-  //       }
-  //     } catch (err) {
-  //       console.log("Không có phiên làm việc hợp lệ.");
-  //     }
-  //   };
-  //   checkSession();
-  // }, [navigate,isRedirecting]);
+  useEffect(() => {
+    if (isRedirecting) return;
+    const checkSession = async () => {
+      try {
+        const response = await axios.get('/api/session');
+        if (response.data && typeof response.data === 'object' && response.data.loggedIn === true) {
+          console.log("Đã đăng nhập, chuyển sang Dashboard");
+          navigate('/dashboard');
+        } else {
+          console.log("Chưa đăng nhập, ở lại trang Login");
+        }
+      } catch (err) {
+        console.log("Không có phiên làm việc hợp lệ.");
+      }
+    };
+    checkSession();
+  }, [navigate,isRedirecting]);
   const handleSocialLogin = (url) => {
     console.log("URL chuyển hướng:", url);
-    setIsRedirecting(true); // Đánh dấu đang chuyển hướng
+    setIsRedirecting(true);
     window.location.href = url;
   };
   const handleChange = (e) => {
