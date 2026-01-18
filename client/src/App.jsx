88a390d68f4881b4c7399b9f8fed1aac8368514b
Nguyen Xuan The <xuanthe01656@gmail.com>
2026-01-19 06:46:36 +0700

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { useState, useEffect, useCallback } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async'; // THÊM DÒNG NÀY
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
 );
